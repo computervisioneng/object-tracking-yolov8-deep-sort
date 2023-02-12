e33c9f442ef607853ac0d0ad2b3799d91b1a92f6
computervisiondeveloper <github@felipetambasco.com>
2023-02-12 10:23:35 -0300

    Initial commit.

---
+++ main.py
@@ -0,0 +1,56 @@
+import os
+import random
+
+import cv2
+from ultralytics import YOLO
+
+from tracker import Tracker
+
+
+video_path = os.path.join('.', 'data', 'people.mp4')
+video_out_path = os.path.join('.', 'out.mp4')
+
+cap = cv2.VideoCapture(video_path)
+ret, frame = cap.read()
+
+cap_out = cv2.VideoWriter(video_out_path, cv2.VideoWriter_fourcc(*'MP4V'), cap.get(cv2.CAP_PROP_FPS),
+                          (frame.shape[1], frame.shape[0]))
+
+model = YOLO("yolov8n.pt")
+
+tracker = Tracker()
+
+colors = [(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)) for j in range(10)]
+
+detection_threshold = 0.5
+while ret:
+
+    results = model(frame)
+
+    for result in results:
+        detections = []
+        for r in result.boxes.data.tolist():
+            x1, y1, x2, y2, score, class_id = r
+            x1 = int(x1)
+            x2 = int(x2)
+            y1 = int(y1)
+            y2 = int(y2)
+            class_id = int(class_id)
+            if score > detection_threshold:
+                detections.append([x1, y1, x2, y2, score])
+
+        tracker.update(frame, detections)
+
+        for track in tracker.tracks:
+            bbox = track.bbox
+            x1, y1, x2, y2 = bbox
+            track_id = track.track_id
+
+            cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (colors[track_id % len(colors)]), 3)
+
+    cap_out.write(frame)
+    ret, frame = cap.read()
+
+cap.release()
+cap_out.release()
+cv2.destroyAllWindows()
---
+++ tracker.py
@@ -0,0 +1,59 @@
+from deep_sort.deep_sort.tracker import Tracker as DeepSortTracker
+from deep_sort.tools import generate_detections as gdet
+from deep_sort.deep_sort import nn_matching
+from deep_sort.deep_sort.detection import Detection
+import numpy as np
+
+
+class Tracker:
+    tracker = None
+    encoder = None
+    tracks = None
+
+    def __init__(self):
+        max_cosine_distance = 0.4
+        nn_budget = None
+
+        encoder_model_filename = 'model_data/mars-small128.pb'
+
+        metric = nn_matching.NearestNeighborDistanceMetric("cosine", max_cosine_distance, nn_budget)
+        self.tracker = DeepSortTracker(metric)
+        self.encoder = gdet.create_box_encoder(encoder_model_filename, batch_size=1)
+
+    def update(self, frame, detections):
+
+        bboxes = np.asarray([d[:-1] for d in detections])
+        bboxes[:, 2:] = bboxes[:, 2:] - bboxes[:, 0:2]
+        scores = [d[-1] for d in detections]
+
+        features = self.encoder(frame, bboxes)
+
+        dets = []
+        for bbox_id, bbox in enumerate(bboxes):
+            dets.append(Detection(bbox, scores[bbox_id], features[bbox_id]))
+
+        self.tracker.predict()
+        self.tracker.update(dets)
+        self.update_tracks()
+
+    def update_tracks(self):
+        tracks = []
+        for track in self.tracker.tracks:
+            if not track.is_confirmed() or track.time_since_update > 1:
+                continue
+            bbox = track.to_tlbr()
+
+            id = track.track_id
+
+            tracks.append(Track(id, bbox))
+
+        self.tracks = tracks
+
+
+class Track:
+    track_id = None
+    bbox = None
+
+    def __init__(self, id, bbox):
+        self.track_id = id
+        self.bbox = bbox
